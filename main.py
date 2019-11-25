846f596e5e1097eb7bf306c69d3b0f3b0d0ed611
Rok Povsic <rok.povsic@gmail.com>
2019-11-25 18:11:05 +0100

    Dodal prikaz uporabnika

--- main.py
+++ main.py
@@ -165,6 +165,12 @@ def uporabniki():
     return render_template("uporabniki.html", uporabniki=users)
 
 
+@app.route("/uporabniki/<uporabnik_id>")
+def prikaz_uporabnika(uporabnik_id):
+    uporabnik = db.query(Uporabnik).filter_by(id=uporabnik_id).first()
+    return render_template("prikaz_uporabnika.html", uporabnik=uporabnik)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 

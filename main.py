bd39b7ac661b5bb1ff71b1af4e85465de361853c
Rok Povsic <rok.povsic@gmail.com>
2019-11-29 07:24:32 +0100

    Dodal prikaz vremena za katerokoli mesto

--- main.py
+++ main.py
@@ -190,23 +190,26 @@ def prikaz_uporabnika(uporabnik_id):
 
 @app.route("/vreme")
 def vreme():
-    mesto = "Vrhnika"
+    mesto = request.args.get("lokacija")
 
-    odgovor_geo = json.loads(requests.get("https://geocode.xyz/" + mesto + "?json=1").text)
-    lon = odgovor_geo["longt"]
-    lat = odgovor_geo["latt"]
+    if mesto:
+        odgovor_geo = json.loads(requests.get("https://geocode.xyz/" + mesto + "?json=1").text)
+        lon = odgovor_geo["longt"]
+        lat = odgovor_geo["latt"]
 
-    url = "https://opendata.si/vreme/report/?lat=" + lat + "&lon=" + lon
+        url = "https://opendata.si/vreme/report/?lat=" + lat + "&lon=" + lon
 
-    # Alternativni načini
-    # url = "https://opendata.si/vreme/report/?lat=%s&lon=%s" % (lat, lon)
-    # url = "https://opendata.si/vreme/report/?lat={}&lon={}".format(lat, lon)
-    # url = f"https://opendata.si/vreme/report/?lat={lat}&lon={lon}"
+        # Alternativni načini
+        # url = "https://opendata.si/vreme/report/?lat=%s&lon=%s" % (lat, lon)
+        # url = "https://opendata.si/vreme/report/?lat={}&lon={}".format(lat, lon)
+        # url = f"https://opendata.si/vreme/report/?lat={lat}&lon={lon}"
 
-    odgovor = json.loads(requests.get(url).text)
-    dez = odgovor["forecast"]["data"][0]["rain"]
+        odgovor = json.loads(requests.get(url).text)
+        dez = odgovor["forecast"]["data"][0]["rain"]
+    else:
+        dez = None
 
-    return render_template("vreme.html", vreme=dez)
+    return render_template("vreme.html", vreme=dez, mesto=mesto)
 
 
 if __name__ == '__main__':
